f59de671b1ec0b2c7d7a8dc86f36218237a44db2
Max Miller <maxmiller413@gmail.com>
2021-03-21 15:29:14 -0500

    commit tree 77c38b17c545e66b5d97132b62764e36a93aa8fb

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.querySelector('main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+newHeader.className = "victory"
